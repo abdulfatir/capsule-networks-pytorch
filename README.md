5d67b7b24d0b697aa8090ecd583060418788fbd4
abdulfatir <abdulfatirs@gmail.com>
2018-02-17 02:49:13 +0800

    Added Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Capsule Networks (PyTorch Implementation)
+
+PyTorch implementation of **Dynamic Routing Between Capsules**<sup>[1]</sup>. This repository includes experiments on MNIST, FashionMNIST, and CIFAR10.
+
+### Credits
+Some general ideas of implementation have been borrowed from [gram-ai/capsule-networks](https://github.com/gram-ai/capsule-networks).
+
+### References
+[1] Sara Sabour, Nicholas Frosst, Geoffrey E Hinton. *Dynamic Routing Between Capsules*, NIPS 2017.
--- models/capsnet.py
+++ models/capsnet.py
@@ -8,14 +8,14 @@ from layers import PrimaryCapsule, Capsule
 
 
 class CapsNet(torch.nn.Module):
-    def __init__(self, in_channels, num_primary_capsules, input_shape=[1, 28, 28]):
+    def __init__(self, num_primary_capsules, input_shape=[1, 28, 28], n_classes=10):
         super(CapsNet, self).__init__()
 
         self.CUDA = torch.cuda.is_available()
         self.input_shape = input_shape
-        self.conv1 = torch.nn.Conv2d(in_channels, 256, 9)
+        self.conv1 = torch.nn.Conv2d(input_shape[0], 256, 9)
         self.primary_capsule = PrimaryCapsule(256, 32, 8, 9, 2)
-        self.digit_capsule = Capsule(num_primary_capsules, 8, 10, 16)
+        self.digit_capsule = Capsule(num_primary_capsules, 8, n_classes, 16)
         self.decoder = nn.Sequential(
             nn.Linear(16 * 10, 512),
             nn.ReLU(inplace=True),
